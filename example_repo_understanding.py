cbc29fd0595a131b25ae38bed7df555ee82a9673
Mchlbohn <138714205+Mchlbohn@users.noreply.github.com>
2023-08-28 15:04:07 -0400

    Update example_repo_understanding.py

--- example_repo_understanding.py
+++ example_repo_understanding.py
@@ -1,4 +1,6 @@
-Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print("I'm understanidng how to use GitHub to keep track fo changes!")
-I'm understanidng how to use GitHub to keep track fo changes!
+#creating a project for GitHub
+#Michael Bohn
+
+print("COP2002 Fall 2023")
+
++print("I'm understanding how to use GitHub to keep track of changes!")
